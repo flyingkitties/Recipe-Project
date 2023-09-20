1ffd9dcee5f13ab72b1797e0a5306c792a214f94
flyingkitties <110839612+flyingkitties@users.noreply.github.com>
2023-09-20 13:10:11 +0100

    update bg image AVIF

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable spaced-comment */
 /* eslint-disable global-require */
 /* eslint-disable import/no-extraneous-dependencies */
 const withMT = require('@material-tailwind/react/utils/withMT');
@@ -20,8 +21,7 @@ module.exports = withMT({
       },
       display: ['group-hover'],
       backgroundImage: {
-        orange:
-          "url('https://i.postimg.cc/SsnjK213/53201018528-cacd95222a-o.webp')",
+        orange: "url('/images/bgAVIF.avif')",
       },
     },
   },
@@ -33,3 +33,5 @@ module.exports = withMT({
     require('@tailwindcss/line-clamp'),
   ],
 });
+
+//https://live.staticflickr.com/65535/52764822208_5a7e6ff762_o.jpg
